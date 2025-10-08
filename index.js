e8bae82b37a98efd88f4f75c10da2484789d5231
Silas Holland <106703852+SilasHolland-KnightMoves@users.noreply.github.com>
2025-10-08 14:36:33 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var sum = 1 + 4;
+ 
+var difference = sum - 2;
+ 
+var product = difference * 5;
+ 
+var quotient = product / 2;
+ 
+var remainder = product % 2;
+ 
+document.getElementById('result').innerHTML = quotient;
+ 
